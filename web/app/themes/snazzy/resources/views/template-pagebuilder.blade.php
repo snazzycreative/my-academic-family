789895621acad2790216d6f7631686978521272e
Steve Ross <steve@snazzycreative.com>
2023-07-26 23:48:42 +0100

    adding pagebuilder acf groups

---
+++ web/app/themes/snazzy/resources/views/template-pagebuilder.blade.php
@@ -0,0 +1,13 @@
+{{--
+  Template Name: Page Builder
+  Template Post Type: page
+--}}
+
+@extends('layouts.app')
+
+@section('content')
+  @while(have_posts()) @php(the_post())
+    @include('partials.page-header')
+    @include('partials.content-page')
+  @endwhile
+@endsection
