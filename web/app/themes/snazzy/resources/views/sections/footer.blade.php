4dbc8beb8243d5971af78cb04abd832c65f241da
Steve Ross <steve@snazzycreative.com>
2023-08-05 22:21:36 +0100

    responsive footer styling

--- web/app/themes/snazzy/resources/views/sections/footer.blade.php
+++ web/app/themes/snazzy/resources/views/sections/footer.blade.php
@@ -6,8 +6,8 @@
     </a>
 
     @if (has_nav_menu('footer_navigation'))
-      <nav class="nav-footer" aria-label="{{ wp_get_nav_menu_name('footer_navigation') }}">
-        {!! wp_nav_menu(['theme_location' => 'footer_navigation', 'menu_class' => 'nav nolist', 'echo' => false]) !!}
+      <nav class="nav-footer-wrap" aria-label="{{ wp_get_nav_menu_name('footer_navigation') }}">
+        {!! wp_nav_menu(['theme_location' => 'footer_navigation', 'menu_class' => 'nav-footer nolist', 'echo' => false]) !!}
       </nav>
     @endif
   </div>
@@ -18,7 +18,7 @@
 
       @if($privacy)
       <div class="privacy">
-        <p><span>|</span> {!! $privacy !!}</p></p>
+        <p>{!! $privacy !!}</p></p>
       </div>
       @endif
 
