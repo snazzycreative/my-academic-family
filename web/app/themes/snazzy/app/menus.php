da20c3eb59e213885d21543a328eeac71807c0a1
Steve Ross <steve@snazzycreative.com>
2023-09-06 22:57:19 +0100

    remove unnecessary composer view

--- web/app/themes/snazzy/app/menus.php
+++ web/app/themes/snazzy/app/menus.php
@@ -7,7 +7,7 @@ add_filter( 'wp_nav_menu_items', __NAMESPACE__.'\\footer', 10, 2);
 function footer( $items, $args ){
 
     if( $args->theme_location == 'footer_navigation' ):
-        $items .= \Roots\view('partials/footer-contact');
+        $items .= \Roots\view('partials/footer-contact', ['contact' => get_page_by_path('contact')]);
     endif;
 
     return $items;
--- web/app/themes/snazzy/resources/views/front-page.blade.php
+++ web/app/themes/snazzy/resources/views/front-page.blade.php
@@ -1,21 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-  @while(have_posts()) @php(the_post())
+  @if(is_front_page())
+    @include('sections.slides.slides')
+  @else
+    @include('sections.banner.banner')
+  @endif
 
-    @if(is_front_page())
-      @include('sections.slides.slides')
-    @else
-      @include('sections.banner.banner')
-    @endif
-
-    @if(get_the_content())
-      <section class="section section-content bg-white">
-        <div class="container container-small">
-          @includeFirst(['partials.content-page', 'partials.content'])
-        </div>
-      </section>
-    @endif
-    @include('partials.pagebuilder')
-  @endwhile
+  @if(get_the_content())
+    <section class="section section-content bg-white">
+      <div class="container container-small">
+        @includeFirst(['partials.content-page', 'partials.content'])
+      </div>
+    </section>
+  @endif
+  @include('partials.pagebuilder')
 @endsection
